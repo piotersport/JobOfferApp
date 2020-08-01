9e8da0137b39afe495ad7b73493468c286cb4719
unknown <pioterofficjal@gmail.com>
2020-08-01 12:11:01 +0200

    innitial commit

---
+++ src/main/java/pl/sda/JobOfficeApp/JobOfficeAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.JobOfficeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfficeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfficeAppApplication.class, args);
+	}
+
+}
